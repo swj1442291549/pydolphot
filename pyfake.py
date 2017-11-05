847a0a13361508f14171bcd64aa03f340766d263
weijia-Server <swj1442291549@gmail.com>
2017-11-05 10:32:33 +0800

    Correct generate_fake

--- pyfake.py
+++ pyfake.py
@@ -3,27 +3,69 @@ import pandas as pd
 import numpy as np
 from astropy.io import fits
 import argparse
+import glob
 
 
-def generate_fakelist(chip_num, filter1, filter2, num):
+def kroupa_mf(ms):
+    p = np.zeros_like(ms)
+    for i, m in enumerate(ms):
+        if m >= 0.5:
+            p[i] = m**-2.35
+        elif m >= 0.08:
+            p[i] = 2 * m**-1.35
+        else:
+            p[i] = 25 * m**-0.35
+    return p
+
+
+def pick_iso():
+    iso_files = sorted(glob.glob("isochrone/*.dat"))
+    filenames = [j.replace('isochrone/', "") for j in iso_files]
+    for i, j in enumerate(filenames):
+        print("{0}: {1}".format(i, j))
+    index = input('Use which isochrone file as the reference: ')
+    if index.isdigit():
+        index = int(index)
+    else:
+        raise ValueError('Please input an integer!')
+    return iso_files[index]
+
+
+def generate_fakelist(iso_file, chip_num, filter1, filter2, num_input, dm):
     hdu_list = fits.open('final/o.gst.fits')
     data = hdu_list[1].data
-    df = pd.DataFrame(np.array(data).byteswap().newbyteorder())
-    df_chip = df[df['chip'] == chip_num]
+    df_o = pd.DataFrame(np.array(data).byteswap().newbyteorder())
+    df_chip = df_o[df_o['chip'] == chip_num]
+    X_min = min(df_chip['X'])
+    Y_min = min(df_chip['Y'])
+    X_max = max(df_chip['X'])
+    Y_max = max(df_chip['Y'])
+
+    f1_mag_max = max(df_chip['{0}_VEGA'.format(filter1)])
+    df_iso = pd.read_table(
+        iso_file, comment='#', delim_whitespace=True)
+    df = df_iso[df_iso['log(age/yr)'] == 9.21]
+    df = df[df[filter1] < f1_mag_max + 1 - dm]
+    df = df.assign(num=np.random.poisson(
+        (kroupa_mf(df['M_ini'])) * num_input / sum(kroupa_mf(df['M_ini']))))
+    num = sum(df['num'])
+    hdu_list = fits.open('final/o.gst.fits')
+    data = hdu_list[1].data
+    df_o = pd.DataFrame(np.array(data).byteswap().newbyteorder())
+    df_chip = df_o[df_o['chip'] == chip_num]
     X_min = min(df_chip['X'])
     Y_min = min(df_chip['Y'])
     X_max = max(df_chip['X'])
     Y_max = max(df_chip['Y'])
     X_fake = np.random.uniform(X_min, X_max, num)
     Y_fake = np.random.uniform(Y_min, Y_max, num)
-    f1_list = [19, 20, 21, 22]
-    c12_list = [0.25, 0.5, 0.75, 1, 1.25, 1.5]
+    num_i = 0
     with open('fake{0}.list'.format(chip_num), 'w') as f:
-        for f1_mag in f1_mag:
-            for c12 in c12_list:
-                for i in range(num):
-                    f.write('0 1 {0} {1} {2} {3}\n'.format(
-                        X_fake[i], Y_fake[i], f1_mag, f1_mag - c12))
+        for i in range(len(df)):
+            for index_item in range(int(df.iloc[i]['num'])):
+                f.write('0 1 {0} {1} {2} {3}\n'.format(X_fake[num_i], Y_fake[
+                    num_i], df.iloc[i][filter1] + dm, df.iloc[i][filter2] + dm))
+                num_i += 1
 
 
 def generate_fake_param(chip_num):
@@ -47,15 +89,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("Bfilter", help='Blue Filter name')
     parser.add_argument("Rfilter", help='Red Filter name')
+    parser.add_argument("dm", type=float, help='Distance Modulus')
     parser.add_argument(
-        '-n', type=int, default=1000, help='Number of fake stars')
+        '-n', type=int, default=10000, help='Number of fake stars')
     args = parser.parse_args()
     filter1 = args.Bfilter
     filter2 = args.Rfilter
+    dm = args.dm
     num = args.n
 
-    generate_fakelist(1, filter1, filter2, num)
-    generate_fakelist(2, filter1, filter2, num)
+    iso_file = pick_iso()
+
+    generate_fakelist(iso_file, 1, filter1, filter2, num, dm)
+    generate_fakelist(iso_file, 2, filter1, filter2, num, dm)
 
     generate_fake_param(1)
     generate_fake_param(2)
