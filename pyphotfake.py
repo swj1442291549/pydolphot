d35c758faa6176aa9e66addf9f0822f74ffbe48b
weijia-Server <swj1442291549@gmail.com>
2017-11-27 19:35:22 +0800

    Fix index_array

--- pyphotfake.py
+++ pyphotfake.py
@@ -70,7 +70,8 @@ if __name__ == "__main__":
     filter_labels = [
         '_VEGA', '_ERR', '_SNR', '_SHARP', '_ROUND', '_CROWD', '_FLAG'
     ]
-    index_array = np.array([31, 33, 35, 36, 37, 38, 39])
+    nimg = len(glob.glob('*flt.fits'))
+    index_array = np.array([15, 17, 19, 20, 21, 22, 23]) + 2 * (nimg + 2)
     labels_list = list()
     for label in filter_labels:
         labels_list.append('{0}{1}'.format(filter1, label))
@@ -109,25 +110,25 @@ if __name__ == "__main__":
                 df = df.append(item.append(data_series), ignore_index=True)
         return df
 
-    pool = Pool(20)
-    result = pool.map(inner_extract, output_names)
-    pool.close()
+    # pool = Pool(20)
+    # result = pool.map(inner_extract, output_names)
+    # pool.close()
 
-    df = pd.concat(result)
-    df.reset_index(drop=True, inplace=True)
+    # df = pd.concat(result)
+    # df.reset_index(drop=True, inplace=True)
 
-    print('Selecting ...')
-    snr = 5
-    sharp = 0.04
-    crowd = 0.5
-    flag = np.zeros(len(df))
+    # print('Selecting ...')
+    # snr = 5
+    # sharp = 0.04
+    # crowd = 0.5
+    # flag = np.zeros(len(df))
 
-    index = (df['{0}_SNR'.format(filter1)] > snr) & (df['{0}_SNR'.format(filter2)] > snr) & (df['{0}_SHARP'.format(filter1)] ** 2 < sharp) & (df['{0}_SHARP'.format(filter2)] ** 2 < sharp) & (df['{0}_CROWD'.format(filter1)] < crowd) & (df['{0}_CROWD'.format(filter2)] < crowd)
+    # index = (df['{0}_SNR'.format(filter1)] > snr) & (df['{0}_SNR'.format(filter2)] > snr) & (df['{0}_SHARP'.format(filter1)] ** 2 < sharp) & (df['{0}_SHARP'.format(filter2)] ** 2 < sharp) & (df['{0}_CROWD'.format(filter1)] < crowd) & (df['{0}_CROWD'.format(filter2)] < crowd)
 
-    df = df.assign(flag=index.values)
+    # df = df.assign(flag=index.values)
 
-    print('Saving ...')
+    # print('Saving ...')
 
-    df.to_pickle('f.{0}.pickle'.format(folder))
+    # df.to_pickle('f.{0}.pickle'.format(folder))
 
-    subprocess.call('mv f.{0}.pickle final'.format(folder), shell=True)
+    # subprocess.call('mv f.{0}.pickle final'.format(folder), shell=True)
