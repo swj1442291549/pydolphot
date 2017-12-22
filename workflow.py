2fa168b052407720953d6367d9983bd31f84eae5
weijia-MBP <swj1442291549@gmail.com>
2017-12-22 10:07:12 +0800

    Add force dol to workflow

--- workflow.py
+++ workflow.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     index = args.index
 
     subprocess.call('python $dol/download.py {0}'.format(index), shell=True)
-    subprocess.call('python $dol/dol.py', shell=True)
+    subprocess.call('python $dol/dol.py --force', shell=True)
     subprocess.call('python $dol/phot.py', shell=True)
     subprocess.call('python $dol/comp.py', shell=True)
     subprocess.call('python $dol/fake.py', shell=True)
