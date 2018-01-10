0aad968d2630824d35ae75997f438229371b1203
weijia-Server <swj1442291549@gmail.com>
2018-01-10 12:42:31 +0800

    Remove useless code

--- phot.py
+++ phot.py
@@ -86,21 +86,12 @@ if __name__ == "__main__":
     objtype = 1
     flag = 99
 
-    wgood = np.where(
-        (t[filters[0] + '_SNR'] >= snr) & (t[filters[1] + '_SNR'] >= snr) &
-        (t[filters[0] + '_SHARP']**2 < sharp) &
-        (t[filters[1] + '_SHARP']**2 < sharp) &
-        (t[filters[0] + '_CROWD'] < crowd) & (
-            t[filters[1] + '_CROWD'] < crowd) & (t['OBJECT_TYPE'] == objtype) &
-        (t[filters[0] + '_FLAG'] <= flag) & (t[filters[1] + '_FLAG'] <= flag))
-
     wgood_list = list()
     for i in range(nfilters):
         wgood = np.where((t[filters[i] + '_SNR'] >= snr) & (
             t[filters[i] + '_SHARP']**2 < sharp
         ) & (t[filters[i] + '_CROWD'] < crowd) & (t['OBJECT_TYPE'] == objtype) &
                          (t[filters[i] + '_FLAG'] <= flag))
-        wgood_list.append(wgood[0])
     cnt = Counter(np.concatenate(wgood_list))
     wgood_index = [k for k, v in cnt.items() if v >= nfilters]
 
