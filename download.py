76be5a4242dbbe68579a99d1de99f9c514cd7b94
weijia-Server <swj1442291549@gmail.com>
2018-04-02 15:45:02 +0800

    Fix for target number smaller than 5 digits

--- download.py
+++ download.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     parser.add_argument('index', type=int, help='Index of anonymous')
     args = parser.parse_args()
     index = args.index
-    proc = subprocess.Popen(["curl", "-l", "ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0}/".format(index)], stdout=subprocess.PIPE)
+    proc = subprocess.Popen(["curl", "-l", "ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/".format(index)], stdout=subprocess.PIPE)
     (out, err) = proc.communicate()
     files = out.decode('utf8').split('\n')
     files.remove('')
 
     for filename in files:
         print('Downloading {0} / {1} ...'.format(files.index(filename), len(files)))
-        subprocess.call("axel -a -n 20 ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0}/{1}".format(index, filename), shell=True)
+        subprocess.call("axel -a -n 20 ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/{1}".format(index, filename), shell=True)
 
     subprocess.call('mkdir raw', shell=True)
     subprocess.call('mv *.fits raw', shell=True)
