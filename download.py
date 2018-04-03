97bf91c7bc96a884bda38691da34003c2cf4e717
weijia-MBP <swj1442291549@gmail.com>
2018-04-03 10:08:16 +0800

    Add number of connnections parameter to download

--- download.py
+++ download.py
@@ -5,8 +5,11 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('index', type=int, help='Index of anonymous')
+    parser.add_argument(
+        '-c', type=int, default=10, help='Number of connections (10)')
     args = parser.parse_args()
     index = args.index
+    num_con = args.c
     proc = subprocess.Popen(["curl", "-l", "ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/".format(index)], stdout=subprocess.PIPE)
     (out, err) = proc.communicate()
     files = out.decode('utf8').split('\n')
@@ -14,7 +17,7 @@ if __name__ == '__main__':
 
     for filename in files:
         print('Downloading {0} / {1} ...'.format(files.index(filename), len(files)))
-        subprocess.call("axel -a -n 20 ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/{1}".format(index, filename), shell=True)
+        subprocess.call("axel -a -n {2} ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/{1}".format(index, filename, num_con), shell=True)
 
     subprocess.call('mkdir raw', shell=True)
     subprocess.call('mv *.fits raw', shell=True)
