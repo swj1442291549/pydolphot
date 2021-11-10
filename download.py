37f296e26c9f2c64f3d39ff13e65a1e85be16949
Weijia Sun <swj1442291549@gmail.com>
2021-11-10 20:28:12 +0800

    Update new HST archive ftp

--- download.py
+++ download.py
@@ -1,62 +1,25 @@
 import os
 import argparse
 import subprocess
-
-
-def which(program):
-    def is_exe(fpath):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-    return None
+import ftplib
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("index", type=int, help="Index of anonymous")
-    parser.add_argument("-c", type=int, default=10, help="Number of connections (10)")
     args = parser.parse_args()
-    index = args.index
-    num_con = args.c
-    proc = subprocess.Popen(
-        [
-            "curl",
-            "-l",
-            "ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/".format(index),
-        ],
-        stdout=subprocess.PIPE,
-    )
-    (out, err) = proc.communicate()
-    files = out.decode("utf8").split("\n")
-    files.remove("")
 
-    if which("axel") is None:
-        print("axel is not found. Use wget instead.")
-        for filename in files:
-            print("Downloading {0} / {1} ...".format(files.index(filename), len(files)))
-            subprocess.call(
-                "wget ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/{1}".format(
-                    index, filename
-                ),
-                shell=True,
-            )
-    else:
-        for filename in files:
-            print("Downloading {0} / {1} ...".format(files.index(filename), len(files)))
-            subprocess.call(
-                "axel -a -n {2} ftp://stdatu.stsci.edu/stage/anonymous/anonymous{0:0>5}/{1}".format(
-                    index, filename, num_con
-                ),
-                shell=True,
-            )
+    ftps = ftplib.FTP_TLS('archive.stsci.edu')
+    ftps.login(user="anonymous", passwd="mail")
+    ftps.prot_p() # This is a really good idea :)
+    ftps.cwd('stage')
+    ftps.cwd('anonymous/anonymous{0:0>d}'.format(args.index)) # stagedir is something like 'anonymous/anonyumous12345'
+
+    filenames = ftps.nlst()
+    for filename in filenames:
+        print("getting " + filename)
+        with open(filename, 'wb') as fp: 
+            ftps.retrbinary('RETR {}'.format(filename), fp.write)
 
     subprocess.call("mkdir raw", shell=True)
     subprocess.call("mv *.fits raw", shell=True)
